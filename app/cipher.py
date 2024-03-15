46fd0ae57de43fa91d3f44688e8f7ac5836b7d06
pkmntrainerfrost <smaarif.achsien@gmail.com>
2024-03-15 20:49:37 +0700

    fix: comments

--- app/cipher.py
+++ app/cipher.py
@@ -201,347 +201,347 @@ if __name__=="__main__":
 
 # Fungsi-fungsi standard vigenere cipher
 
-def vigenereEncrypt(plaintext:str, key:str):
+# def vigenereEncrypt(plaintext:str, key:str):
 
-    plaintext_preprocessed = preprocess(plaintext)
-    key_preprocessed = preprocess(key)
+#     plaintext_preprocessed = preprocess(plaintext)
+#     key_preprocessed = preprocess(key)
 
-    ciphertext = ""
-    i = 0
+#     ciphertext = ""
+#     i = 0
 
-    for char in plaintext_preprocessed:
+#     for char in plaintext_preprocessed:
 
-        ciphertext += chr(((ord(char) + ord(key_preprocessed[i])) % 26) + ord("A"))
+#         ciphertext += chr(((ord(char) + ord(key_preprocessed[i])) % 26) + ord("A"))
 
-        i = (i + 1) % (len(key_preprocessed))
+#         i = (i + 1) % (len(key_preprocessed))
 
-    return ciphertext
+#     return ciphertext
 
-def vigenereDecrypt(ciphertext:str, key:str):
+# def vigenereDecrypt(ciphertext:str, key:str):
 
-    ciphertext_preprocessed = preprocess(ciphertext)
-    key_preprocessed = preprocess(key)
+#     ciphertext_preprocessed = preprocess(ciphertext)
+#     key_preprocessed = preprocess(key)
 
-    plaintext = ""
-    i = 0
+#     plaintext = ""
+#     i = 0
 
-    for char in ciphertext_preprocessed:
+#     for char in ciphertext_preprocessed:
 
-        plaintext += chr(((ord(char) - ord(key_preprocessed[i])) % 26) + ord("A"))
+#         plaintext += chr(((ord(char) - ord(key_preprocessed[i])) % 26) + ord("A"))
 
-        i = (i + 1) % (len(key_preprocessed))
+#         i = (i + 1) % (len(key_preprocessed))
 
-    return plaintext
+#     return plaintext
 
-# Fungsi-fungsi extended vigenere cipher (WIP)
+# # Fungsi-fungsi extended vigenere cipher (WIP)
 
-def vigenereExtendedEncrypt(plaintext:str, key:str):
+# def vigenereExtendedEncrypt(plaintext:str, key:str):
 
 
-    ciphertext = ""
-    i = 0
+#     ciphertext = ""
+#     i = 0
 
-    for char in plaintext:
+#     for char in plaintext:
 
-        ciphertext += chr(((ord(char) + ord(key[i])) % 256))
+#         ciphertext += chr(((ord(char) + ord(key[i])) % 256))
 
-        i = (i + 1) % (len(key))
+#         i = (i + 1) % (len(key))
 
-    return ciphertext
+#     return ciphertext
 
-def vigenereExtendedDecrypt(ciphertext:str, key:str):
+# def vigenereExtendedDecrypt(ciphertext:str, key:str):
 
-    plaintext = ""
-    i = 0
+#     plaintext = ""
+#     i = 0
 
-    for char in ciphertext:
+#     for char in ciphertext:
 
-        plaintext += chr(((ord(char) - ord(key[i])) % 256))
+#         plaintext += chr(((ord(char) - ord(key[i])) % 256))
 
-        i = (i + 1) % (len(key))
+#         i = (i + 1) % (len(key))
 
-    return plaintext
+#     return plaintext
 
-def vigenereExtendedEncryptBytes(plainbytes, key:str):
+# def vigenereExtendedEncryptBytes(plainbytes, key:str):
 
-    ciphertext = b""
-    i = 0
+#     ciphertext = b""
+#     i = 0
 
-    while (byte := plainbytes.read(1)):
+#     while (byte := plainbytes.read(1)):
 
-        ciphertext += ((int.from_bytes(byte,"little") + ord(key[i])) % 256).to_bytes(1,"little")
+#         ciphertext += ((int.from_bytes(byte,"little") + ord(key[i])) % 256).to_bytes(1,"little")
 
-        i = (i + 1) % (len(key))
+#         i = (i + 1) % (len(key))
 
-    return ciphertext
+#     return ciphertext
 
-def vigenereExtendedDecryptBytes(cipherbytes, key:str):
+# def vigenereExtendedDecryptBytes(cipherbytes, key:str):
 
-    plaintext = b""
-    i = 0
+#     plaintext = b""
+#     i = 0
 
-    while (byte := cipherbytes.read(1)):
+#     while (byte := cipherbytes.read(1)):
 
         
-        plaintext += ((int.from_bytes(byte,"little") - ord(key[i])) % 256).to_bytes(1,"little")
+#         plaintext += ((int.from_bytes(byte,"little") - ord(key[i])) % 256).to_bytes(1,"little")
 
-        i = (i + 1) % (len(key))
+#         i = (i + 1) % (len(key))
 
-    return plaintext
+#     return plaintext
 
-# Fungsi-fungsi playfair cipher
+# # Fungsi-fungsi playfair cipher
 
-def playfairGenerateKeyMatrix(key:str):
+# def playfairGenerateKeyMatrix(key:str):
 
-    key_matrix = [["" for i in range(5)] for j in range(5)]
-    key_dict = {}
-    key_preprocessed = "".join(dict.fromkeys(preprocess(key).replace("J","")))
+#     key_matrix = [["" for i in range(5)] for j in range(5)]
+#     key_dict = {}
+#     key_preprocessed = "".join(dict.fromkeys(preprocess(key).replace("J","")))
 
-    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+#     alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 
-    i, j = 0, 0
-    for char in key_preprocessed:
+#     i, j = 0, 0
+#     for char in key_preprocessed:
 
-        key_matrix[i][j] = char
-        key_dict[char] = (i,j)
+#         key_matrix[i][j] = char
+#         key_dict[char] = (i,j)
 
-        j += 1
+#         j += 1
 
-        if j >= 5:
-            j = 0
-            i += 1
+#         if j >= 5:
+#             j = 0
+#             i += 1
         
-        alphabet = alphabet.replace(char,"")
+#         alphabet = alphabet.replace(char,"")
 
-    for char in alphabet:
+#     for char in alphabet:
 
-        key_matrix[i][j] = char
-        key_dict[char] = (i,j)
+#         key_matrix[i][j] = char
+#         key_dict[char] = (i,j)
 
-        j += 1
+#         j += 1
 
-        if j >= 5:
-            j = 0
-            i += 1
+#         if j >= 5:
+#             j = 0
+#             i += 1
 
-    return key_matrix, key_dict
+#     return key_matrix, key_dict
 
-def playfairEncrypt(plaintext:str, key:str):
+# def playfairEncrypt(plaintext:str, key:str):
     
-    key_matrix, key_dict = playfairGenerateKeyMatrix(key)
-    plaintext_preprocessed = preprocess(plaintext).replace("J","I")
+#     key_matrix, key_dict = playfairGenerateKeyMatrix(key)
+#     plaintext_preprocessed = preprocess(plaintext).replace("J","I")
 
-    plaintext_padded = ""
-    second_letter = 0
+#     plaintext_padded = ""
+#     second_letter = 0
 
-    for char in plaintext_preprocessed:
+#     for char in plaintext_preprocessed:
 
-        if not second_letter:
-            plaintext_padded += char
-            second_letter = True
+#         if not second_letter:
+#             plaintext_padded += char
+#             second_letter = True
 
-        else:
-            if plaintext_padded[-1] == char:
-                plaintext_padded += ("X " if plaintext_padded[-1] != "X " else "Z ")
-                plaintext_padded += char
-            else:
-                plaintext_padded += char + " "
-                second_letter = False
+#         else:
+#             if plaintext_padded[-1] == char:
+#                 plaintext_padded += ("X " if plaintext_padded[-1] != "X " else "Z ")
+#                 plaintext_padded += char
+#             else:
+#                 plaintext_padded += char + " "
+#                 second_letter = False
 
-    if second_letter:
-        plaintext_padded += "X"
+#     if second_letter:
+#         plaintext_padded += "X"
 
-    bigrams = plaintext_padded.split(" ")
-    ciphertext = ""
+#     bigrams = plaintext_padded.split(" ")
+#     ciphertext = ""
 
-    bigrams = [bigram for bigram in bigrams if bigram != ""]
+#     bigrams = [bigram for bigram in bigrams if bigram != ""]
 
-    for bigram in bigrams:
+#     for bigram in bigrams:
 
-        first_i, first_j = key_dict[bigram[0]]
-        second_i, second_j = key_dict[bigram[1]]
+#         first_i, first_j = key_dict[bigram[0]]
+#         second_i, second_j = key_dict[bigram[1]]
 
-        bigram_ciphertext = ""
+#         bigram_ciphertext = ""
 
-        if first_i == second_i:
+#         if first_i == second_i:
 
-            bigram_ciphertext += key_matrix[first_i][(first_j + 1) % 5]
-            bigram_ciphertext += key_matrix[second_i][(second_j + 1) % 5]
+#             bigram_ciphertext += key_matrix[first_i][(first_j + 1) % 5]
+#             bigram_ciphertext += key_matrix[second_i][(second_j + 1) % 5]
 
-        elif first_j == second_j:
+#         elif first_j == second_j:
 
-            bigram_ciphertext += key_matrix[(first_i + 1) % 5][first_j]
-            bigram_ciphertext += key_matrix[(second_i + 1) % 5][second_j]
+#             bigram_ciphertext += key_matrix[(first_i + 1) % 5][first_j]
+#             bigram_ciphertext += key_matrix[(second_i + 1) % 5][second_j]
 
-        else:
+#         else:
 
-            bigram_ciphertext += key_matrix[first_i][second_j]
-            bigram_ciphertext += key_matrix[second_i][first_j]
+#             bigram_ciphertext += key_matrix[first_i][second_j]
+#             bigram_ciphertext += key_matrix[second_i][first_j]
         
-        ciphertext += bigram_ciphertext
+#         ciphertext += bigram_ciphertext
     
-    return ciphertext
+#     return ciphertext
 
-def playfairDecrypt(ciphertext:str, key:str):
+# def playfairDecrypt(ciphertext:str, key:str):
 
-    key_matrix, key_dict = playfairGenerateKeyMatrix(key)
-    ciphertext_preprocessed = preprocess(ciphertext).replace("J","I")
+#     key_matrix, key_dict = playfairGenerateKeyMatrix(key)
+#     ciphertext_preprocessed = preprocess(ciphertext).replace("J","I")
 
-    bigrams = [ciphertext_preprocessed[i:i + 2] for i in range(0, len(ciphertext_preprocessed), 2)]
-    plaintext = ""
+#     bigrams = [ciphertext_preprocessed[i:i + 2] for i in range(0, len(ciphertext_preprocessed), 2)]
+#     plaintext = ""
 
-    bigrams = [bigram for bigram in bigrams if bigram != ""]
+#     bigrams = [bigram for bigram in bigrams if bigram != ""]
 
-    for bigram in bigrams:
+#     for bigram in bigrams:
 
-        first_i, first_j = key_dict[bigram[0]]
-        second_i, second_j = key_dict[bigram[1]]
+#         first_i, first_j = key_dict[bigram[0]]
+#         second_i, second_j = key_dict[bigram[1]]
 
-        bigram_plaintext = ""
+#         bigram_plaintext = ""
 
-        if first_i == second_i:
+#         if first_i == second_i:
 
-            bigram_plaintext += key_matrix[first_i][(first_j - 1) % 5]
-            bigram_plaintext += key_matrix[second_i][(second_j - 1) % 5]
+#             bigram_plaintext += key_matrix[first_i][(first_j - 1) % 5]
+#             bigram_plaintext += key_matrix[second_i][(second_j - 1) % 5]
 
-        elif first_j == second_j:
+#         elif first_j == second_j:
 
-            bigram_plaintext += key_matrix[(first_i - 1) % 5][first_j]
-            bigram_plaintext += key_matrix[(second_i - 1) % 5][second_j]
+#             bigram_plaintext += key_matrix[(first_i - 1) % 5][first_j]
+#             bigram_plaintext += key_matrix[(second_i - 1) % 5][second_j]
 
-        else:
+#         else:
 
-            bigram_plaintext += key_matrix[first_i][second_j]
-            bigram_plaintext += key_matrix[second_i][first_j]
+#             bigram_plaintext += key_matrix[first_i][second_j]
+#             bigram_plaintext += key_matrix[second_i][first_j]
         
-        plaintext += bigram_plaintext
+#         plaintext += bigram_plaintext
 
-    return plaintext
+#     return plaintext
 
-# Fungsi-fungsi product cipher
+# # Fungsi-fungsi product cipher
 
-def productEncrypt(plaintext:str, vigenere_key:str, transposition_key:int):
+# def productEncrypt(plaintext:str, vigenere_key:str, transposition_key:int):
 
-    vigenere_ciphertext = vigenereEncrypt(plaintext,vigenere_key)
+#     vigenere_ciphertext = vigenereEncrypt(plaintext,vigenere_key)
 
-    transposition_matrix = [["" for i in range(transposition_key)] for i in range(-(len(vigenere_ciphertext) // -transposition_key))]
-    ciphertext = ""
+#     transposition_matrix = [["" for i in range(transposition_key)] for i in range(-(len(vigenere_ciphertext) // -transposition_key))]
+#     ciphertext = ""
 
-    i, j = 0, 0
-    for char in vigenere_ciphertext:
+#     i, j = 0, 0
+#     for char in vigenere_ciphertext:
 
-        transposition_matrix[i][j] = char
+#         transposition_matrix[i][j] = char
 
-        j += 1
+#         j += 1
 
-        if j >= transposition_key:
-            j = 0
-            i += 1
+#         if j >= transposition_key:
+#             j = 0
+#             i += 1
 
-    for j in range(j,transposition_key):
+#     for j in range(j,transposition_key):
 
-        transposition_matrix[i][j] = "X"
+#         transposition_matrix[i][j] = "X"
 
-    for j in range(transposition_key):
+#     for j in range(transposition_key):
 
-        for i in range(len(transposition_matrix)):
+#         for i in range(len(transposition_matrix)):
 
-            ciphertext += transposition_matrix[i][j]
+#             ciphertext += transposition_matrix[i][j]
 
-    return ciphertext
+#     return ciphertext
 
-def productDecrypt(ciphertext:str, vigenere_key:str, transposition_key:int):
+# def productDecrypt(ciphertext:str, vigenere_key:str, transposition_key:int):
 
-    transposition_matrix = [["" for i in range(-(len(ciphertext) // -transposition_key))] for i in range(transposition_key)]
-    product_plaintext = ""
+#     transposition_matrix = [["" for i in range(-(len(ciphertext) // -transposition_key))] for i in range(transposition_key)]
+#     product_plaintext = ""
 
-    i, j = 0, 0
-    for char in ciphertext:
+#     i, j = 0, 0
+#     for char in ciphertext:
 
-        transposition_matrix[i][j] = char
+#         transposition_matrix[i][j] = char
 
-        j += 1
+#         j += 1
 
-        if j >= (len(transposition_matrix[0])):
-            j = 0
-            i += 1
+#         if j >= (len(transposition_matrix[0])):
+#             j = 0
+#             i += 1
 
 
-    for j in range(len(transposition_matrix[0])):
+#     for j in range(len(transposition_matrix[0])):
 
-        for i in range(transposition_key):
+#         for i in range(transposition_key):
 
-            product_plaintext += transposition_matrix[i][j]
+#             product_plaintext += transposition_matrix[i][j]
     
-    plaintext = vigenereDecrypt(product_plaintext,vigenere_key)
+#     plaintext = vigenereDecrypt(product_plaintext,vigenere_key)
 
-    return plaintext
+#     return plaintext
 
-# Fungsi-fungsi affine cipher
+# # Fungsi-fungsi affine cipher
 
-def affineEncrypt(plaintext:str, m:int, b:int):
+# def affineEncrypt(plaintext:str, m:int, b:int):
 
-    plaintext_preprocessed = preprocess(plaintext)
+#     plaintext_preprocessed = preprocess(plaintext)
 
-    ciphertext = ""
+#     ciphertext = ""
 
-    for char in plaintext_preprocessed:
+#     for char in plaintext_preprocessed:
 
-        c = chr(((m * (ord(char) - ord("A")) + b) % 26) + ord("A"))
-        ciphertext += c
+#         c = chr(((m * (ord(char) - ord("A")) + b) % 26) + ord("A"))
+#         ciphertext += c
 
-    return ciphertext
+#     return ciphertext
 
-def affineDecrypt(ciphertext:str, m:int, b:int):
+# def affineDecrypt(ciphertext:str, m:int, b:int):
 
-    ciphertext_preprocessed = preprocess(ciphertext)
+#     ciphertext_preprocessed = preprocess(ciphertext)
 
-    plaintext = ""
+#     plaintext = ""
 
-    for char in ciphertext_preprocessed:
+#     for char in ciphertext_preprocessed:
 
-        p = chr(((pow(m, -1, 26) * (ord(char) - ord("A") - b)) % 26) + ord("A"))
-        plaintext += p
+#         p = chr(((pow(m, -1, 26) * (ord(char) - ord("A") - b)) % 26) + ord("A"))
+#         plaintext += p
     
-    return plaintext
+#     return plaintext
 
-# Fungsi-fungsi autokey vigenere
+# # Fungsi-fungsi autokey vigenere
 
-def vigenereAutokeyEncrypt(plaintext:str, key:str):
+# def vigenereAutokeyEncrypt(plaintext:str, key:str):
 
-    plaintext_preprocessed = preprocess(plaintext)
-    key_preprocessed = preprocess(key)
+#     plaintext_preprocessed = preprocess(plaintext)
+#     key_preprocessed = preprocess(key)
 
-    ciphertext = ""
-    i = 0
+#     ciphertext = ""
+#     i = 0
 
-    for char in plaintext_preprocessed:
+#     for char in plaintext_preprocessed:
 
-        c = chr(((ord(char) + ord(key_preprocessed[i])) % 26) + ord("A"))
+#         c = chr(((ord(char) + ord(key_preprocessed[i])) % 26) + ord("A"))
 
-        ciphertext += c
-        key_preprocessed += char
+#         ciphertext += c
+#         key_preprocessed += char
 
-        i += 1
+#         i += 1
 
-    return ciphertext
+#     return ciphertext
 
-def vigenereAutokeyDecrypt(ciphertext:str, key:str):
+# def vigenereAutokeyDecrypt(ciphertext:str, key:str):
 
-    ciphertext_preprocessed = preprocess(ciphertext)
-    key_preprocessed = preprocess(key)
+#     ciphertext_preprocessed = preprocess(ciphertext)
+#     key_preprocessed = preprocess(key)
 
-    plaintext = ""
-    i = 0
+#     plaintext = ""
+#     i = 0
 
-    for char in ciphertext_preprocessed:
+#     for char in ciphertext_preprocessed:
 
-        p = chr(((ord(char) - ord(key_preprocessed[i])) % 26) + ord("A"))
+#         p = chr(((ord(char) - ord(key_preprocessed[i])) % 26) + ord("A"))
 
-        plaintext += p
-        key_preprocessed += p
+#         plaintext += p
+#         key_preprocessed += p
 
-        i += 1
+#         i += 1
 
-    return plaintext
+#     return plaintext
 
